e396964113d2e34f7ffb90cb30c5f6303e825d26
tracer <tracer@tracer.org>
2018-03-26 20:31:50 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- crepl.c
+++ crepl.c
@@ -48,7 +48,7 @@ int main()
 			 break;
 		}
 		else{		//如果是表达式，如果是字母开头是函数，如果是数字开头是表达式
-			char *expr_name = "__expr_wrap_";
+			char expr_name[] = "__expr_wrap_";
 			printf("this is before sprintf\n");
 			sprintf(expr_name, "%s%d", expr_name, cmd_id);
 			printf("expr_name:%s\n", expr_name);
