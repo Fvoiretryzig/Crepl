e0183d690c0780734cd1b2772e3fa66bdb99ccb2
tracer <tracer@tracer.org>
2018-03-26 20:37:48 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- crepl.c
+++ crepl.c
@@ -33,7 +33,7 @@ void *func_lookup(char *name)
 int main() 
 {
 	/*创建文件，用来存放代码*/
-	char *cmd_so = "gcc temp_code.c -shared -fPIC -o temp_code.so";
+	char *cmd_so = "gcc temp_code.c -shared -fPIC -o temp_code.so -ldl";
 	FILE *fp = fopen(filename, "a+");
 	printf(">> ");
 	while(fgets(code, sizeof(code), stdin) != NULL){
---
+++ temp_code.c
@@ -0,0 +1,2 @@
+int __expr_wrap_0(){return 1+1
+;}
