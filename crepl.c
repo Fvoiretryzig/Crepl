f4deffdb6b757a56b249b7b6b4f95d56bf842c05
tracer <tracer@tracer.org>
2018-03-26 21:18:47 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- crepl.c
+++ crepl.c
@@ -38,7 +38,7 @@ int main()
 {
 	/*创建文件，用来存放代码*/
 	char *cmd_so = "gcc temp_code.c -shared -fPIC -o temp_code.so -ldl";
-	FILE *fp = fopen(filename, "r+");
+	FILE *fp = fopen(filename, "a+");
 	printf(">> ");
 	while(fgets(code, sizeof(code), stdin) != NULL){
 		//printf("this is at 40 lines code:%s\n", code);
@@ -74,6 +74,9 @@ int main()
 			}
 			int value = func(); // 通过函数指针调用
 			printf(">> %s = %d.\n", code, value);	
+			fclose(fp);
+			fp = fopen(filename, "w");
+			fclose(fp);
 			dlclose(handle);	
 		}
 		printf(">> ");
--- temp_code.c
+++
@@ -1,6 +0,0 @@
-int __expr_wrap_0(){return 2+2;}
-int __expr_wrap_1(){return 2+2;}
-int __expr_wrap_2(){return 2+2;}
-int __expr_wrap_3(){return 2+2;}
-int __expr_wrap_4(){return 2+2;}
-int __expr_wrap_5(){return 2+2;}
