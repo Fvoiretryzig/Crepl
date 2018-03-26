e297f2a4222fba2e18cea14e2b84ba1d4dad2fd7
tracer <tracer@tracer.org>
2018-03-26 20:57:27 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- temp_code.c
+++ temp_code.c
@@ -1,12 +1,1 @@
-int __expr_wrap_0(){return 1+1
-;}
-int __expr_wrap_0(){return 1+1
-;}
-int __expr_wrap_1(){return 2+1
-;}
-int __expr_wrap_0(){return 1+1
-;}
-int __expr_wrap_1(){return 
-;}
-int __expr_wrap_2(){return 
-;}
+int __expr_wrap_0(){return 1+1;}
