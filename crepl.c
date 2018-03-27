8878cb7abd442d65f130e1ef19885f1fdc7fe53b
tracer <tracer@tracer.org>
2018-03-27 09:03:55 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- crepl.c
+++ crepl.c
@@ -60,6 +60,7 @@ int main()
 			}	
 			int (*func)() = func_lookup(expr_name); // 查找XXX对应的函数
 			printf("this is after lookup\n");
+			printf("this is func:%s\n", func);
 			if(func == 0){
 				printf("\n>> ");
 				continue;
