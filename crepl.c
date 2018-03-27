72639c1336838d65f5b83c5cb11dfdf9da0a99b7
tracer <tracer@tracer.org>
2018-03-27 09:44:33 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- crepl.c
+++ crepl.c
@@ -5,19 +5,25 @@
 #include<string.h>
 
 char code[2048];
-char *filename = "temp_code.c";
-char *libname = "temp_code.so";
+char *filename = "code.c";
+char *libname = "code.so";
+//如果编译不报错才把正确的代码加入到code.c中
+char *check_filename = "check_code.c";
+char *check_libname = "check_code.so";
 int cmd_id = 0;
 
 void *handle;
 
-void *func_lookup(char *name)
+void *func_lookup(char *name, int if_check)
 {
 	//void *handle;  
     char *error;  
           
     int (*func )();  
-    handle = dlopen("./temp_code.so", RTLD_LAZY);  
+    if(if_check)
+    	handle = dlopen("./check_code.so", RTLD_LAZY);
+    else
+	    handle = dlopen("./code.so", RTLD_LAZY);  
     if(!handle){  
 		fputs(dlerror(), stderr);  
 		return 0;
@@ -33,7 +39,8 @@ void *func_lookup(char *name)
 int main() 
 {
 	/*创建文件，用来存放代码*/
-	char *cmd_so = "gcc temp_code.c -Werror -shared -fPIC -o temp_code.so -ldl";
+	char *cmd_so = "gcc code.c -Werror -shared -fPIC -o code.so -ldl";
+	char *cmd_check = "gcc check_code.c -Werror -shard -fPIC -o check_code.so -ldl";
 	FILE *fp = fopen(filename, "w+");
 	printf(">> ");
 	while(fgets(code, sizeof(code), stdin) != NULL){
@@ -48,30 +55,46 @@ int main()
 			 break;
 		}
 		else{		//如果是表达式，如果是字母开头是函数，如果是数字开头是表达式
+			FILE *check_fp = fopen(check_filename, "w+");
 			char expr_name[32] = "__expr_wrap_";
-			sprintf(expr_name, "%s%d", expr_name, cmd_id++);
+			char check_name[32] = "check";
 			code[strlen(code)-1] = '\0';
-			fprintf(fp, "int %s(){return %s;}\n", expr_name, code);
+			fprintf(check_fp, "int %s(){return %s;}", check_name, code);
 			fflush(fp);
-			if(system(cmd_so)){		//把求值变为函数再加入到动态库中
+			if(system(cmd_check)){
+				printf("errors while checking\n");
+				printf(">> ");
+				fclose(check_filename);
+				continue;
+			}
+			else{
+				sprintf(expr_name, "%s%d", expr_name, cmd_id++);
+				fprintf(fp, "int %s(){return %s;}\n", expr_name, code);
+				fflush(fp);	
+				system(cmd_so);
+				int (*func)() = func_lookup(expr_name, 0); // 查找XXX对应的函数
+				if(func == 0){
+					printf("\n>> ");
+					continue;
+				int value = func(); // 通过函数指针调用
+				printf(">> %s = %d.\n", code, value);	
+				dlclose(handle);					
+			}			
+						
+			}
+
+			/*if(system(cmd_so)){		//把求值变为函数再加入到动态库中
 				printf("error while linking\n");
 				printf(">> ");
 				continue;
-			}	
-			int (*func)() = func_lookup(expr_name); // 查找XXX对应的函数
-			//printf("this is after lookup\n");
-			//printf("this is func:0x%x\n", func);
-			if(func == 0){
-				printf("\n>> ");
-				continue;
-			}
-			int value = func(); // 通过函数指针调用
-			printf(">> %s = %d.\n", code, value);	
-			dlclose(handle);	
+			}*/	
+
+	
 		}
 		printf(">> ");
 	}
 	fclose(fp); remove(filename);
+	
 	remove(libname);
 	return 0;
 }
