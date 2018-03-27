b67934ee24062a4639511b86819df8cdea501caf
tracer <tracer@tracer.org>
2018-03-27 17:53:42 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- crepl.c
+++ crepl.c
@@ -7,9 +7,6 @@
 char code[2048];
 char *filename = "code.c";
 char *libname = "code.so";
-//如果编译不报错才把正确的代码加入到code.c中
-//char *check_filename = "check_code.c";
-//char *check_libname = "check_code.so";
 int cmd_id = 0;
 
 void *handle;
@@ -21,7 +18,7 @@ void *func_lookup(char *name)
           
     int (*func )();  
     handle = dlopen("./code.so", RTLD_LAZY);  
-    if(!handle){  	
+    if(!handle){  
 		fputs(dlerror(), stderr);  
 		return 0;
 	}      
@@ -37,7 +34,6 @@ int main()
 {
 	/*创建文件，用来存放代码*/
 	char *cmd_so = "gcc code.c -Werror -shared -fPIC -o code.so -ldl";
-	//char *cmd_check = "gcc check_code.c -Werror -shared -fPIC -o check_code.so -ldl";
 	FILE *fp = fopen(filename, "w+");
 	printf(">> ");
 	while(fgets(code, sizeof(code), stdin) != NULL){
@@ -51,34 +47,31 @@ int main()
 		else if(code[0] == 'e' && code[1] == 'x' && code[2] == 'i' && code[3] == 't'){
 			 break;
 		}
-		else{		
+		else{		//如果是表达式，如果是字母开头是函数，如果是数字开头是表达式
 			char expr_name[32] = "__expr_wrap_";
-			code[strlen(code)-1] = '\0';
 			sprintf(expr_name, "%s%d", expr_name, cmd_id++);
+			code[strlen(code)-1] = '\0';
 			fprintf(fp, "int %s(){return %s;}\n", expr_name, code);
-			fflush(fp);	
+			fflush(fp);
 			if(system(cmd_so)){		//把求值变为函数再加入到动态库中
 				printf("error while linking\n");
 				printf(">> ");
 				continue;
-			}
+			}	
 			int (*func)() = func_lookup(expr_name); // 查找XXX对应的函数
+			//printf("this is after lookup\n");
+			//printf("this is func:0x%x\n", func);
 			if(func == 0){
 				printf("\n>> ");
 				continue;
+			}
 			int value = func(); // 通过函数指针调用
 			printf(">> %s = %d.\n", code, value);	
-			dlclose(handle);					
-			}			
-						
-			//}	
-
-	
+			dlclose(handle);	
 		}
 		printf(">> ");
 	}
 	fclose(fp); remove(filename);
-	//remove(check_filename);
 	remove(libname);
 	return 0;
 }
