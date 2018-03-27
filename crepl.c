b3c4a1505a4362f3ec34ae66e8a3bf4349a08994
tracer <tracer@tracer.org>
2018-03-27 08:32:59 +0800

    >  compile forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- crepl.c
+++ crepl.c
@@ -52,8 +52,9 @@ int main()
 		else{		//如果是表达式，如果是字母开头是函数，如果是数字开头是表达式
 			char expr_name[32] = "__expr_wrap_";
 			//printf("this is before sprintf\n");
-			sprintf(expr_name, "%s%d", expr_name, cmd_id);
+			sprintf(expr_name, "%s%d", expr_name, cmd_id++);
 			printf("expr_name:%s\n", expr_name);
+			code[strlen(code)-1] = "\0";
 			fprintf(fp, "int %s(){return %s;}\n", expr_name, code);
 			printf("this is before system\n");
 			if(system(cmd_so)){		//把求值变为函数再加入到动态库中
